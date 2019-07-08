6e8c518e046df78ac67344c26e0ad7042f2d9622
Emmanuel Neni <emmanuelneni@gmail.com>
2019-07-08 18:41:10 +0300

    added some sleep time spacings

--- app.py
+++ app.py
@@ -25,21 +25,25 @@ class TwitterBot:
     def like_tweet(self, hashtag):
         bot = self.bot
         bot.get('https://twitter.com/search?q='+hashtag+'&src=typd')
-        time.sleep(3)
-        for i in range(1,3):
+        time.sleep(5)
+        for i in range(1, 5):
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(2)
+            time.sleep(10)
             tweets = bot.find_elements_by_class_name('tweet')
-            links = [elem.get_attribute('data-permalink-path') 
-                    for elem in tweets]
+            links = [elem.get_attribute('data-permalink-path')
+                     for elem in tweets]
         for link in links:
             bot.get('https://twitter.com' + link)
+            time.sleep(15)
             try:
                 bot.find_element_by_class_name('HeartAnimation').click()
-                time.sleep(20)
+                bot.execute_script(
+                    'window.scrollTo(0,document.body.scrollHeight)')
+                time.sleep(30)
             except Exception as ex:
                 time.sleep(60)
 
+
 ed = TwitterBot('@neniemmanuel', '14003417twitter')
 ed.login()
-ed.like_tweet('bitcoin')
+ed.like_tweet('myfirstTweet')
