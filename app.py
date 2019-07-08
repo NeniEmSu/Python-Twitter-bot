bf7f62ec157f17b0e10402320aba40263025cda1
cryptodoctor <emmanuelneni@gmail.com>
2019-07-08 13:55:54 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def like_tweet(self, hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/seatch?q='+hashtag+'&src=typd')
+        bot.sleep(3)
+
+
+ed = TwitterBot('@neniemmanuel', '14003417twitter')
+ed.login()
+ed.like_tweet('webdev')
